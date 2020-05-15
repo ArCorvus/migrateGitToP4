7e2be8891453072f8e1ea4e66e18f9f9b690df49
Vladimir Ivanov <v.o.ivanov@gmail.com>
2020-05-15 09:21:54 +0300

    update

--- migrateGitToP4.sh
+++ migrateGitToP4.sh
@@ -2,10 +2,13 @@
 
 export P4PORT=192.168.1.1:1666
 export P4USER=user1
+export P4PASSWD=123456
 export P4CLIENT=CLIENT_FOR_MIGRATION_FROM_GIT
 
-P4_PATH=~/my_p4_repo
-SCRIPT_PATH=$(pwd)
+export P4_PATH=~/my_p4_repo
+export GIT_PATH=~/my_git_repo
+export SCRIPT_PATH=$(pwd)
+echo $GIT_PATH
 
 cd $P4_PATH
 p4 status > /dev/null
@@ -15,4 +18,5 @@ then
   return
 fi
 
-git filter-branch --index-filter '$SCRIPT_PATH/migrate_step.sh $GIT_COMMIT "$GIT_AUTHOR_NAME"'
+cd $GIT_PATH
+git filter-branch --index-filter '$SCRIPT_PATH/migrate_step.sh $GIT_COMMIT "$GIT_COMMITTER_NAME", "$GIT_COMMITTER_DATE"'
--- migrate_step.sh
+++ migrate_step.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-GIT_PATH=~/my_git_repo
+cd $P4_PATH
 
 git --git-dir=$GIT_PATH/.git reset --hard $1
 GIT_MSG=$(git log --format=%B -n 1 HEAD)
 GIT_AUTHOR=$2
+GIT_COMMIT_DATE=$2
 
 p4 reconcile //depot/...
-p4 submit -d "$GIT_AUTHOR: $GIT_MSG"
+p4 submit -d "$GIT_AUTHOR: $GIT_MSG, $GIT_COMMIT_DATE"
